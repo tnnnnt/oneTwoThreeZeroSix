32d8ac33cb0ec26fe4ca432f4ac179b33d341b28
tnnnnt <cyz2324141281@163.com>
2024-10-05 14:02:43 +0800

    read json

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
-print("Hello World")
+import json
+fs = '普宁'
+ts = '深圳'
+date = '2024-10-05'
+with open('train.json', 'r', encoding='utf-8') as file:
+    train_data = json.load(file)
+with open('city.json', 'r', encoding='utf-8') as file:
+    city_data = json.load(file)
+print(train_data)
+print(city_data)
