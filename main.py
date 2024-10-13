2208fe6c96de45afbd5c4a4cbac712684b7f0bdd
tnnnnt <cyz2324141281@163.com>
2024-10-13 16:43:23 +0800

    区分了票面出发站、票面到达站、实际到达站

--- main.py
+++ main.py
@@ -2,11 +2,15 @@ import requests
 import json
 import time
 
-# 下一步更新：爬价格、过滤出发时间和到达时间、实际终点
+# 封装一下
+# 票面出发时间、票面到达时间、实际出发时间、实际到达时间
+# 票面价格、是否需要补票、补票后预估价格
+# 过滤出发时间、到达时间、历时、是否需要补票、价格、实际终点
+# 过滤高铁站
 
 from_station = '普宁'
-to_station_city = '深圳'
-depart_date = '2024-10-10'
+to_city = '深圳'
+depart_date = '2024-10-19'
 time_sleep = 5
 chance = 3
 headers = {
@@ -25,7 +29,7 @@ with open('train.json', 'r', encoding='utf-8') as file:
 	train_data = json.load(file)
 with open('city.json', 'r', encoding='utf-8') as file:
 	city_data = json.load(file)
-to_station = city_data[to_station_city][0]
+to_station = city_data[to_city][0]
 from_station_telecode = train_data[from_station]['telecode']
 to_station_telecode = train_data[to_station]['telecode']
 url_query_g = "https://kyfw.12306.cn/otn/leftTicket/queryG"
@@ -56,18 +60,18 @@ for data_query_g in datas_query_g:
 	train_no = tmp[2]
 	to_station_telecode = tmp[7]
 	has_tickets = tmp[11]
+	for from_station_in_ticket, v in train_data.items():
+		if v["telecode"] == to_station_telecode:
+			to_station = from_station_in_ticket
+			break
 	if has_tickets == 'Y':
 		train_number = tmp[3]
 		start_time = tmp[8]
 		end_time = tmp[9]
 		duration_time = tmp[10]
 		ticket_stock_and_price = tmp[39]
-		for k, v in train_data.items():
-			if v["telecode"] == to_station_telecode:
-				to_station = k
-				break
-		print('车次: {}, 出发站: {}, 到达站: {}, 出发时间: {}, 到达时间: {}, 历时: {}'.format(
-			train_number, from_station, to_station, start_time, end_time, duration_time))
+		print('车次: {}, 票面出发站: {}, 票面到达站: {}, 实际到达站: {}, 出发时间: {}, 到达时间: {}, 历时: {}'.format(
+			train_number, from_station, to_station, to_station, start_time, end_time, duration_time))
 		for i in range(0, len(ticket_stock_and_price), 10):
 			t = ticket_stock_and_price[i:i+10]
 			if t[7:] != '000':
@@ -95,13 +99,16 @@ for data_query_g in datas_query_g:
 				station_names.append(data_station_name['station_name'])
 			for from_ind in range(from_station_ind + 1):
 				if station_names[from_ind] not in possible_train_datas:
-					possible_train_datas[station_names[from_ind]] = {'train_no_list': [], 'to_station_list': []}
-				possible_train_datas[station_names[from_ind]]['train_no_list'].append(train_no)
+					possible_train_datas[station_names[from_ind]] = {}
+				# possible_train_datas[station_names[from_ind]]['train_no_list'].append(train_no)
 				for to_ind in range(from_station_ind + 1, len(station_names)):
 					if station_names[to_ind] == '汕  头':  # 就你汕头搞特殊，害我出bug！
 						station_names[to_ind] = '汕头'
-					if station_names[to_ind] not in possible_train_datas[station_names[from_ind]]['to_station_list']:
-						possible_train_datas[station_names[from_ind]]['to_station_list'].append(station_names[to_ind])
+					if station_names[to_ind] not in possible_train_datas[station_names[from_ind]]:
+						possible_train_datas[station_names[from_ind]][station_names[to_ind]] = {}
+					if train_no not in possible_train_datas[station_names[from_ind]][station_names[to_ind]]:
+						possible_train_datas[station_names[from_ind]][station_names[to_ind]][train_no] = []
+					possible_train_datas[station_names[from_ind]][station_names[to_ind]][train_no].append(to_station)
 		except ValueError:
 			print('------')
 			print("无法解析 JSON 响应")
@@ -114,13 +121,12 @@ for data_query_g in datas_query_g:
 		print(tmp)
 		print('------')
 		continue
-for k, v in possible_train_datas.items():
-	from_station_telecode_tmp = train_data[k]['telecode']
-	to_station_list = v['to_station_list']
-	for to_station_tmp in to_station_list:
-		to_station_telecode_tmp = train_data[to_station_tmp]['telecode']
-		params_query_g['leftTicketDTO.from_station'] = from_station_telecode_tmp
-		params_query_g['leftTicketDTO.to_station'] = to_station_telecode_tmp
+for from_station_in_ticket, v in possible_train_datas.items():
+	from_station_in_ticket_telecode = train_data[from_station_in_ticket]['telecode']
+	for to_station_in_ticket, vv in v.items():
+		to_station_in_ticket_telecode = train_data[to_station_in_ticket]['telecode']
+		params_query_g['leftTicketDTO.from_station'] = from_station_in_ticket_telecode
+		params_query_g['leftTicketDTO.to_station'] = to_station_in_ticket_telecode
 		chance_no = 1
 		while True:
 			try:
@@ -139,16 +145,15 @@ for k, v in possible_train_datas.items():
 			train_no = tmp[2]
 			to_station_telecode = tmp[7]
 			has_tickets = tmp[11]
-			if train_no not in v[
-				'train_no_list'] or to_station_telecode != to_station_telecode_tmp or has_tickets == 'N':
+			if train_no not in vv or to_station_telecode != to_station_in_ticket_telecode or has_tickets == 'N':
 				continue
 			train_number = tmp[3]
 			start_time = tmp[8]
 			end_time = tmp[9]
 			duration_time = tmp[10]
 			ticket_stock_and_price = tmp[39]
-			print('车次: {}, 出发站: {}, 到达站: {}, 出发时间: {}, 到达时间: {}, 历时: {}'.format(
-				train_number, k, to_station_tmp, start_time, end_time, duration_time))
+			print('车次: {}, 票面出发站: {}, 票面到达站: {}, 实际到达站: {}, 出发时间: {}, 到达时间: {}, 历时: {}'.format(
+				train_number, from_station_in_ticket, to_station_in_ticket, vv[train_no], start_time, end_time, duration_time))
 			for i in range(0, len(ticket_stock_and_price), 10):
 				t = ticket_stock_and_price[i:i+10]
 				if t[7:] != '000':
